8dbaf0f869163fdadadd2e3538e36726c8ea4dd3
Christopher McClellan <ckuhn203@gmail.com>
2016-02-14 15:03:43 -0500

    Added nuspec

--- Rubberduck.Winforms/Properties/AssemblyInfo.cs
+++ Rubberduck.Winforms/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Rubberduck.Winforms")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("DataAnnotation and Validation Framework for Winforms")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Christopher J. McClellan")]
 [assembly: AssemblyProduct("Rubberduck.Winforms")]
 [assembly: AssemblyCopyright("Copyright © Christopher J. McClellan 2016")]
 [assembly: AssemblyTrademark("")]
